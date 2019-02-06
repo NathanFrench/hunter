d1eadea02e67273b842fc31cde3d1ceff7a6a0a7
Nathan French <nate@cl0d.com>
2019-02-05 21:01:37 -0500

    update sha1

--- cmake/projects/libevhtp/hunter.cmake
+++ cmake/projects/libevhtp/hunter.cmake
@@ -9,8 +9,8 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME libevhtp
     VERSION "develop"
-    URL " https://github.com/criticalstack/libevhtp/archive/develop.tar.gz"
-    SHA1 "89a9737a89e37d2cd508de61427f96905b261935")
+    URL " https://github.com/criticalstack/libevhtp/archive/hunter-develop.tar.gz"
+    SHA1 "8ff33195bf5e2a8e56c4144c5e5635e46b0432cd")
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(libevhtp)
